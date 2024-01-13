a5d76893d091a189177f3ef15aaaaa787057ea1f
unknown <alexgall98@mail.ru>
2024-01-13 09:10:36 +0300

    feat: add function for task 3 module 5

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,31 @@
 function initCarousel() {
-  // ваш код...
+  let currentSlide = 0;
+  const slides = document.querySelectorAll('.carousel__inner .carousel__slide');
+  const totalSlides = slides.length;
+  const arrowRight = document.querySelector('.carousel__arrow_right');
+  const arrowLeft = document.querySelector('.carousel__arrow_left');
+  const carouselInner = document.querySelector('.carousel__inner');
+
+  arrowLeft.style.display ='none';
+
+  function checkArrowsToHide() {  
+    arrowRight.style.display = currentSlide === totalSlides - 1 ? 'none' : '';
+    arrowLeft.style.display = currentSlide === 0 ? 'none' : '';
+  }
+
+  function updateCarouselPosition() {
+    carouselInner.style.transform = `translateX(-${carouselInner.offsetWidth * currentSlide}px)`;
+  }
+
+  arrowRight.addEventListener('click', () => {
+    currentSlide++;
+    updateCarouselPosition();
+    checkArrowsToHide();
+  })
+
+  arrowLeft.addEventListener('click', () => {
+    currentSlide--;
+    updateCarouselPosition();
+    checkArrowsToHide();
+  })
 }
